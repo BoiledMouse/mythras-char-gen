2710304288c39663ee5f878d7c36f65ab2d9faf2
BoiledMouse <46424288+BoiledMouse@users.noreply.github.com>
2025-04-20 16:20:17 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,9 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {
-      // (removed backgroundImage entry; using manual CSS classes instead)
       backgroundImage: {
-        wood: "url('assets/wood.jpg')",
-        parchment: "url('assets/parchment.jpg')",
+        wood: "url('./assets/wood.jpg')",
+        parchment: "url('./assets/parchment.jpg')",
       },
       fontFamily: {
         display: ['"Pirata One"', 'serif'],
