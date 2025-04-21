fd738e5cc6d955fd8a219fb98f4c8486e1299e4f
BoiledMouse <46424288+BoiledMouse@users.noreply.github.com>
2025-04-21 14:29:07 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-// src/App.jsx
 import React from 'react';
+import { CharacterProvider } from './context/characterContext';
 import WizardContainer from './steps/WizardContainer';
 
-function App() {
-  return <WizardContainer />;
+export default function App() {
+  return (
+    <CharacterProvider>
+      <WizardContainer />
+    </CharacterProvider>
+  );
 }
-
-export default App;
